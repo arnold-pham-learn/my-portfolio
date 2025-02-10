c998c81aeda90ab07669a4b731d23794e61b7f11
arnold-pham-learn <arnoldpham.learn@gmail.com>
2025-02-10 11:01:33 +0700

    Update script.js
    I don't know why after update file to github, it loss some code

--- script.js
+++ script.js
@@ -22,4 +22,7 @@ function showPopup(bool) {
   if (bool) {
     document.getElementById('popup').style.visibility = 'visible'
   } else {
-    document.getElementById('popup').style.visibili
+    document.getElementById('popup').style.visibility = 'hidden'
+  }
+}
+
